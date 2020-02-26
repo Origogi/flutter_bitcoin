3d91934e1e1bb387f35ecfad6744333e08876653
Administrator <rlawjdxo1212@naver.com>
2020-02-26 12:38:23 +0900

    complete

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -29,5 +29,22 @@ const List<String> cryptoList = [
 ];
 
 const apiKey = 'F4ABBF37-E72E-44FD-9AB6-B8F48515C7DD';
+const baseUrl = 'https://rest.coinapi.io/v1/exchangerate';
 
-class CoinData {}
+String getCoinDataUrl(String base, String quote) {
+  String url =  '$baseUrl/$base/$quote?apikey=$apiKey';
+  print(url);
+  return url;
+}
+
+class CoinData {
+  String base;
+  String quote;
+  double rate;
+
+  CoinData(var response) {
+    base = response['asset_id_base'];
+    quote = response['asset_id_quote'];
+    rate = response['rate'];
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,8 +1,8 @@
 import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:bitcoin_ticker/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:io' show Platform;  //at the top
-
+import 'dart:io' show Platform; //at the top
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -11,6 +11,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  List<String> rateList = ['?', '?', '?'];
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -24,11 +25,34 @@ class _PriceScreenState extends State<PriceScreen> {
     return DropdownButton<String>(
       items: dropdownItems,
       value: selectedCurrency,
-      onChanged: (value) {
+      onChanged: (value) async {
         setState(() {
           selectedCurrency = value;
+
+          for (int i = 0; i < 3; i++) {
+            rateList[i] = '?';
+          }
+        });
+
+        for (int i = 0; i < 3; i++) {
+          NetworkHelper networkHelper =
+              NetworkHelper(getCoinDataUrl(cryptoList[i], selectedCurrency));
+          var responseData = await networkHelper.getData();
+          print(responseData);
+
+          if (responseData != null) {
+            CoinData coinData = CoinData(responseData);
+            rateList[i] = coinData.rate.toInt().toString();
+          } else {
+            rateList[i] = '?';
+          }
+        }
+
+        setState(() {
         });
       },
+
+      
     );
   }
 
@@ -52,10 +76,28 @@ class _PriceScreenState extends State<PriceScreen> {
   Widget getPicker() {
     if (Platform.isIOS) {
       return iOSPicker();
-    }
-    else if(Platform.isAndroid){
+    } else if (Platform.isAndroid) {
       return androidDropdown();
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (int i = 0; i < 3; i++) {
+      getCoinData(cryptoList[i], selectedCurrency).then((result) {
+        print("result: $result");
+        updateUI(i, result);
+      });
+    }
+  }
+
+  Future<CoinData> getCoinData(String base, String quote) async {
+    NetworkHelper networkHelper = NetworkHelper(getCoinDataUrl(base, quote));
+    var responseData = await networkHelper.getData();
+    CoinData coinData = CoinData(responseData);
+    return coinData;
   }
 
   @override
@@ -68,35 +110,55 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: getCryptoCurrenciesWidget(),
           ),
           Container(
-              height: 150.0,
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(bottom: 30.0),
-              color: Colors.lightBlue,
-              child: Platform.isIOS ? iOSPicker() : androidDropdown(),)
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+          )
         ],
       ),
     );
+  }
+
+  void updateUI(int index, CoinData coinData) {
+    setState(() {
+      selectedCurrency = coinData.quote;
+      rateList[index] = coinData.rate.round().toString();
+    });
+  }
+
+  List<Widget> getCryptoCurrenciesWidget() {
+    List<Widget> wigets = [];
+
+    for (int i = 0; i < 3; i++) {
+      wigets.add(Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              '1 ${cryptoList[i]} = ${rateList[i]} $selectedCurrency',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ));
+    }
+    return wigets;
   }
 }
